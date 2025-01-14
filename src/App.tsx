9e473961a9d5e23104179913959c45e3627dd46b
Ekaterina Zavertyaeva <katsiarynazavertyaeva@gmail.com>
2025-01-14 22:48:28 +0100

    add lesson17 frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Layout from "./pages/UsersApp/Layout/Layout"
-import Home from "./pages/UsersApp/Home/Home"
-import Users from "./pages/UsersApp/Users/Users"
+// import Layout from "./pages/UsersApp/Layout/Layout"
+// import Home from "./pages/UsersApp/Home/Home"
+// import Users from "./pages/UsersApp/Users/Users"
 
 //lessons
 // import Lesson16 from "./lessons/Lesson16/Lesson16"
+import Lesson17 from "./lessons/Lesson17/Lesson17"
 
 //homeworks
+// import Homework16 from "./homeworks/Homework16/Homework16"
 
 //consultations
 // import Consultation08 from "./consultations/Consultation08/Consultation08"
@@ -19,13 +21,15 @@ const App = () => {
       {/* <Consultation08 /> */}
       {/* <Lesson16 /> */}
       {/* UserApp - Practice - Lesson16 */}
-      <Layout>
+      {/* <Layout>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/users' element={<Users/>}/>
           <Route path='*' element='Page not found'/>
         </Routes>
-      </Layout>
+      </Layout> */}
+      {/* <Homework16 /> */}
+      <Lesson17/>
     </BrowserRouter>
   )
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -3,11 +3,13 @@ import { combineSlices, configureStore } from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/query"
 import { counterSlice } from "./redux/counter/counterSlice"
 import { usersSlice } from "./redux/users/usersSlice"
+import { feedbackSlice } from "./redux/feedback/feedbackSlice"
+import { randomJokesSlice } from "./redux/randomJokes/randomJokesSlice"
 
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
 //8. Передаём созданные слайсы в compineSlices
-const rootReducer = combineSlices(counterSlice, usersSlice)
+const rootReducer = combineSlices(counterSlice, usersSlice, feedbackSlice, randomJokesSlice)
 // Infer the `RootState` type from the root reducer
 export type RootState = ReturnType<typeof rootReducer>
 
